38bbb17f8bfe9e9cbde887bf88c32b3439837127
velopert <public.velopert@gmail.com>
2017-05-04 00:33:43 +0900

    immutable

--- src/components/CounterList.js
+++ src/components/CounterList.js
@@ -11,7 +11,7 @@ const CounterList = ({counters, onIncrement, onDecrement, onSetColor}) => {
             <Counter 
                 key={i}
                 index={i}
-                {...counter}
+                {...counter.toJS()}
                 onIncrement={onIncrement}
                 onDecrement={onDecrement}
                 onSetColor={onSetColor}
